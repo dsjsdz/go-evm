534dd990b8c74bc5cea7638dcdd013a29a7ab13e
cleveng <cleveng@gmail.com>
2024-05-08 10:18:57 +0800

    add test

--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/dsjsdz/go-evm/util"
 	"github.com/panjf2000/gnet/v2"
 	"log"
-	"strconv"
-	"strings"
-	"time"
 )
 
 type echoServer struct {
@@ -29,44 +25,47 @@ func (es *echoServer) OnTraffic(c gnet.Conn) gnet.Action {
 	var (
 		buf, _ = c.Next(-1)
 	)
-	fmt.Printf("%s", buf)
+	fmt.Printf("收到内容: %v\n", string(buf))
 
-	payload := strings.Split(string(buf), " ")
-	if len(payload) == 0 {
-		panic("桢内容格式不正确")
-	}
-
-	// 帧长度 即 index 为 2-3 的内容，3默认是 00，不去计算
-	size, err := strconv.ParseInt(payload[util.FrameLenSize], 16, 64)
-	if err != nil {
-		panic(err)
-	}
-
-	if len(payload) != int(size) {
-		panic("桢内容长度不正确")
-	}
-
-	var (
-		funcCode = payload[4]
-		reply    []byte
-		now      = time.Now()
-		year     = strconv.FormatInt(int64(now.Year()), 16)
-		month    = strconv.FormatInt(int64(now.Month()), 16)
-		day      = strconv.FormatInt(int64(now.Day()), 16)
-		hour     = strconv.FormatInt(int64(now.Hour()), 16)
-		minute   = strconv.FormatInt(int64(now.Minute()), 16)
-		second   = strconv.FormatInt(int64(now.Second()), 16)
-		tmpl     = "0D 24 1D 00 80 %v %v %v %v %v %v %s 00 00 00 00 00 00 00 00 00 00 00 00 00 00 8D 0D 0A"
-	)
-	switch funcCode {
-	case util.MethodServerTimeGet: // 登陆服务器
-		// NOTE: 处理逻辑，FrameMachineIdSize 设备id长度14
-		reply = []byte(fmt.Sprintf(tmpl, year, month, day, hour, minute, second, util.SuccessStatusCode))
-	default:
-		// 其他自行处理
-	}
-
-	go c.Write(reply)
+	//payload := strings.Split(string(buf), " ")
+	//if len(payload) == 0 {
+	//	log.Println("桢内容格式不正确")
+	//	return gnet.None
+	//}
+	//
+	//// 帧长度 即 index 为 2-3 的内容，3默认是 00，不去计算
+	//size, err := strconv.ParseInt(payload[util.FrameLenSize], 16, 64)
+	//if err != nil {
+	//	log.Println(err)
+	//	return gnet.None
+	//}
+	//
+	//if len(payload) != int(size) {
+	//	log.Println("桢内容长度不正确")
+	//	return gnet.None
+	//}
+	//
+	//var (
+	//	funcCode = payload[4]
+	//	reply    []byte
+	//	now      = time.Now()
+	//	year     = strconv.FormatInt(int64(now.Year()), 16)
+	//	month    = strconv.FormatInt(int64(now.Month()), 16)
+	//	day      = strconv.FormatInt(int64(now.Day()), 16)
+	//	hour     = strconv.FormatInt(int64(now.Hour()), 16)
+	//	minute   = strconv.FormatInt(int64(now.Minute()), 16)
+	//	second   = strconv.FormatInt(int64(now.Second()), 16)
+	//	tmpl     = "0D 24 1D 00 80 %v %v %v %v %v %v %s 00 00 00 00 00 00 00 00 00 00 00 00 00 00 8D 0D 0A"
+	//)
+	//switch funcCode {
+	//case util.MethodServerTimeGet: // 登陆服务器
+	//	// NOTE: 处理逻辑，FrameMachineIdSize 设备id长度14
+	//	reply = []byte(fmt.Sprintf(tmpl, year, month, day, hour, minute, second, util.SuccessStatusCode))
+	//default:
+	//	// 其他自行处理
+	//}
+	//
+	//go c.Write(reply)
 	return gnet.None
 }
 
--- test/client_test.go
+++ test/client_test.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"net"
+	"testing"
 )
 
-func main() {
+func Test_Client(t *testing.T) {
 	conn, err := net.Dial("tcp", "localhost:7890")
 	if err != nil {
 		fmt.Println(err)
@@ -14,18 +15,18 @@ func main() {
 	defer conn.Close()
 
 	if _, err = conn.Write([]byte("0D 24 1D 00 80 15 07 16 15 2A 2A 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 8D 0D 0A")); err != nil {
-		fmt.Println(err)
+		t.Log(err)
 		return
 	}
-	fmt.Println("Message sent to server")
+	t.Log("Message sent to server")
 
 	// 读取服务器回复的消息
 	buffer := make([]byte, 1024)
 	n, err := conn.Read(buffer)
 	if err != nil {
-		fmt.Println("Error reading reply:", err)
+		t.Log("Error reading reply:", err)
 		return
 	}
 
-	fmt.Println("Reply from server:", string(buffer[:n]))
+	t.Log("Reply from server:", string(buffer[:n]))
 }
